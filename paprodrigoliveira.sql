4fb5dd2b3d83c438a0fcf57abf2ad7f8b55c81c9
R0drig0liveira572 <a10970@agrcanelas.com>
2026-01-02 15:25:46 +0000

    base de dados - versao 3

--- paprodrigoliveira.sql
+++ paprodrigoliveira.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 30-Dez-2025 às 14:12
+-- Tempo de geração: 02-Jan-2026 às 16:24
 -- Versão do servidor: 10.4.32-MariaDB
 -- versão do PHP: 8.2.12
 
@@ -39,7 +39,6 @@ CREATE TABLE `categorias` (
 
 INSERT INTO `categorias` (`id_categoria`, `nome`, `data`) VALUES
 (1, 'Jardinagem', '2025-12-09 19:35:51'),
-(2, 'Designer', '2025-12-09 19:38:45'),
 (3, 'Bricolage', '2025-12-09 19:38:45'),
 (4, 'Cozinha', '2025-12-12 15:38:27');
 
@@ -55,6 +54,7 @@ CREATE TABLE `servicos` (
   `descricao` varchar(200) NOT NULL,
   `categoria` int(11) NOT NULL,
   `horas` int(11) NOT NULL,
+  `id_prestador` int(11) NOT NULL,
   `data` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -62,9 +62,11 @@ CREATE TABLE `servicos` (
 -- Extraindo dados da tabela `servicos`
 --
 
-INSERT INTO `servicos` (`id_servico`, `nome`, `descricao`, `categoria`, `horas`, `data`) VALUES
-(1, 'Jardinagem', 'cortar mato', 1, 10, '2025-12-12 15:54:36'),
-(2, 'Cozinha', 'lavar pratos', 2, 10, '2025-12-12 16:18:25');
+INSERT INTO `servicos` (`id_servico`, `nome`, `descricao`, `categoria`, `horas`, `id_prestador`, `data`) VALUES
+(1, 'Jardinagem', 'cortar mato', 1, 10, 1, '2025-12-12 15:54:36'),
+(2, 'Cozinha', 'lavar pratos', 3, 10, 2, '2025-12-12 16:18:25'),
+(3, 'Cozinha', 'lavar pratos', 4, 10, 3, '2025-12-30 13:30:49'),
+(5, 'limpar cozinha', 'asasas', 4, 1, 3, '2026-01-02 15:06:05');
 
 -- --------------------------------------------------------
 
@@ -74,13 +76,19 @@ INSERT INTO `servicos` (`id_servico`, `nome`, `descricao`, `categoria`, `horas`,
 
 CREATE TABLE `transacoes` (
   `id_transacao` int(11) NOT NULL,
-  `id_prestador` int(11) NOT NULL,
   `id_receptor` int(11) NOT NULL,
   `id_servico` int(11) NOT NULL,
   `horas_trocadas` int(11) NOT NULL,
   `estado` varchar(50) NOT NULL,
   `data` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Extraindo dados da tabela `transacoes`
+--
+
+INSERT INTO `transacoes` (`id_transacao`, `id_receptor`, `id_servico`, `horas_trocadas`, `estado`, `data`) VALUES
+(1, 2, 1, 4, 'concluido', '2025-12-30 13:47:25');
 
 -- --------------------------------------------------------
 
@@ -105,7 +113,9 @@ CREATE TABLE `utilizadores` (
 
 INSERT INTO `utilizadores` (`id_utilizador`, `nome`, `email`, `password`, `foto`, `creditos`, `perfil`, `data`) VALUES
 (1, 'Martim', 'a10970@agrcanelas.com', 'greyert', '', 80, 1, '2025-12-12 16:03:02'),
-(2, 'Rodrigo', 'rodrigomiguelsolivieira@gmail.com', 'hrhtryrty', 'ere', 12, 1, '2025-12-12 16:36:57');
+(2, 'Rodrigo', 'rodrigomiguelsolivieira@gmail.com', 'hrhtryrty', 'ere', 12, 0, '2025-12-12 16:36:57'),
+(3, 'Hélder', 'helderpso@gmail.com', '06afa6c8b54d3cc80d269379d8b6a078', '', 159, 0, '2025-12-30 14:26:13'),
+(4, 'jOSÉ', 'jose@lol.com', '9eb9a779166a1d7c9312fd91ba1d2def', 'uploads/5d766d2b132e9780c4c5a45a45044a75.jpg', 12, 0, '2025-12-30 17:54:38');
 
 --
 -- Índices para tabelas despejadas
@@ -149,19 +159,19 @@ ALTER TABLE `categorias`
 -- AUTO_INCREMENT de tabela `servicos`
 --
 ALTER TABLE `servicos`
-  MODIFY `id_servico` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_servico` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de tabela `transacoes`
 --
 ALTER TABLE `transacoes`
-  MODIFY `id_transacao` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_transacao` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de tabela `utilizadores`
 --
 ALTER TABLE `utilizadores`
-  MODIFY `id_utilizador` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_utilizador` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
