2938b7c3d9031dc09c3ad33db241fc0c5e5455c1
R0drig0liveira572 <a10970@agrcanelas.com>
2026-01-02 15:55:18 +0000

    homepage frontend (em construção)

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Banco do Tempo</title>
+</head>
+<body>
+    <h1>Banco do Tempo</h1>
+</body>
+</html>
