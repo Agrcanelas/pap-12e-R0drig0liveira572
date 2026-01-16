19378075b2cb8e2384100b568dead496e9ada59f
R0drig0liveira572 <a10970@agrcanelas.com>
2026-01-16 14:40:22 +0000

    Dashboard do utilizador

---
+++ user/index.php
@@ -0,0 +1,228 @@
+<?php
+if (session_status() !== PHP_SESSION_ACTIVE) {
+  session_start();
+}
+include '../config.php';
+
+
+// Get current user ID from session
+$user_id = isset($_SESSION['user_id']) ? (int)$_SESSION['user_id'] : 0;
+
+// Count total serviços for current user
+$total_servicos = 0;
+$sql_count = "SELECT COUNT(*) AS total FROM servicos WHERE id_prestador = ?";
+if ($stmt_count = $conn->prepare($sql_count)) {
+  $stmt_count->bind_param("i", $user_id);
+  if ($stmt_count->execute()) {
+    $stmt_count->bind_result($total_servicos);
+    $stmt_count->fetch();
+    $total_servicos = (int) $total_servicos;
+  }
+  $stmt_count->close();
+} else {
+  // In case of error, keep $total_servicos = 0
+}
+
+// Count total transacoes for current user
+$total_transacoes = 0;
+$sql_count = "SELECT COUNT(*) AS total FROM transacoes WHERE id_receptor = ?";
+if ($stmt_count = $conn->prepare($sql_count)) {
+  $stmt_count->bind_param("i", $user_id);
+  if ($stmt_count->execute()) {
+    $stmt_count->bind_result($total_transacoes);
+    $stmt_count->fetch();
+    $total_transacoes = (int) $total_transacoes;
+  }
+  $stmt_count->close();
+} else {
+  // In case of error, keep $total_transacoes = 0
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<!-- [Head] start -->
+
+<head>
+  <title>Home | Banco do Tempo</title>
+  <!-- [Meta] -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+  <!-- [Favicon] icon -->
+  <link rel="icon" href="../assets/images/favicon.svg" type="image/x-icon"> <!-- [Google Font] Family -->
+  <link rel="stylesheet"
+    href="https://fonts.googleapis.com/css2?family=Public+Sans:wght@300;400;500;600;700&display=swap"
+    id="main-font-link">
+  <!-- [Tabler Icons] https://tablericons.com -->
+  <link rel="stylesheet" href="../assets/fonts/tabler-icons.min.css">
+  <!-- [Feather Icons] https://feathericons.com -->
+  <link rel="stylesheet" href="../assets/fonts/feather.css">
+  <!-- [Font Awesome Icons] https://fontawesome.com/icons -->
+  <link rel="stylesheet" href="../assets/fonts/fontawesome.css">
+  <!-- [Material Icons] https://fonts.google.com/icons -->
+  <link rel="stylesheet" href="../assets/fonts/material.css">
+  <!-- [Template CSS Files] -->
+  <link rel="stylesheet" href="../assets/css/style.css" id="main-style-link">
+
+</head>
+<!-- [Head] end -->
+<!-- [Body] Start -->
+
+<body data-pc-preset="preset-1" data-pc-direction="ltr" data-pc-theme="light">
+  <!-- [ Pre-loader ] start -->
+  <div class="loader-bg">
+    <div class="loader-track">
+      <div class="loader-fill"></div>
+    </div>
+  </div>
+  <!-- [ Pre-loader ] End -->
+  <!-- [ Sidebar Menu ] start -->
+  <nav class="pc-sidebar">
+    <div class="navbar-wrapper">
+      <div class="header">
+        <a href="index.php" class="b-brand text-primary">
+          <!-- ========   Change your logo from here   ============ -->
+          <img src="../assets/images/logotipo.png" class="img-fluid logo-lg" alt="Banco do Tempo">
+        </a>
+      </div>
+      <div class="navbar-content">
+        <ul class="pc-navbar">
+          <li class="pc-item">
+            <a href="index.php" class="pc-link">
+              <span class="pc-micon"><i class="ti ti-dashboard"></i></span>
+              <span class="pc-mtext">Dashboard</span>
+            </a>
+          </li>
+
+
+          <li class="pc-item pc-caption">
+            <label>SERVIÇOS</label>
+            <i class="ti ti-news"></i>
+          </li>
+          <li class="pc-item">
+            <a href="servicos.php" class="pc-link">
+              <span class="pc-micon"><i class="ti ti-list"></i></span>
+              <span class="pc-mtext">Todos os serviços</span>
+            </a>
+          </li>
+          <li class="pc-item">
+            <a href="novo-servico.php" class="pc-link">
+              <span class="pc-micon"><i class="ti ti-layout-grid-add"></i></span>
+              <span class="pc-mtext">Novo serviço</span>
+            </a>
+          </li>
+
+        
+
+          <li class="pc-item pc-caption">
+            <label>TRANSAÇÕES</label>
+            <i class="ti ti-brand-chrome"></i>
+          </li>
+          <li class="pc-item">
+            <a href="transacoes.php" class="pc-link">
+              <span class="pc-micon"><i class="ti ti-arrows-right-left"></i></span>
+              <span class="pc-mtext">Todas as transações</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!-- [ Sidebar Menu ] end -->
+  <!-- [ Header Topbar ] start -->
+  <header class="pc-header">
+    <div class="header-wrapper">
+      <!-- [Mobile Media Block] start -->
+      <div class="me-auto pc-mob-drp">
+        <ul class="list-unstyled">
+          <!-- ======= Menu collapse Icon ===== -->
+          <li class="pc-h-item pc-sidebar-collapse">
+            <a href="#" class="pc-head-link ms-0" id="sidebar-hide">
+              <i class="ti ti-menu-2"></i>
+            </a>
+          </li>
+          <li class="pc-h-item pc-sidebar-popup">
+            <a href="#" class="pc-head-link ms-0" id="mobile-collapse">
+              <i class="ti ti-menu-2"></i>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <!-- [Mobile Media Block end] -->
+      <div class="ms-auto">
+        <ul class="list-unstyled">
+          <li class="dropdown pc-h-item header-user-profile">
+            <?php
+            $user_creditos = isset($_SESSION['user_creditos']) ? htmlspecialchars($_SESSION['user_creditos']) : '0';
+            echo '<i class="ti ti-coin"></i>&nbsp;' . $user_creditos . ' créditos';
+            ?>
+          </li>
+          <li class="dropdown pc-h-item header-user-profile">
+            <a class="pc-head-link dropdown-toggle arrow-none me-0" data-bs-toggle="dropdown" href="#" role="button"
+              aria-haspopup="false" data-bs-auto-close="outside" aria-expanded="false">
+              <i class="ti ti-face-id"></i> <span>Olá <?php
+            $user_name = isset($_SESSION['user_name']) ? htmlspecialchars($_SESSION['user_name']) : 'Utilizador';
+            // Extract first word from user name
+            $name_parts = explode(' ', $user_name);
+            echo $name_parts[0];
+            ?></span>
+            </a>
+            <div class="dropdown-menu dropdown-user-profile dropdown-menu-end pc-h-dropdown">
+              <div class="tab-content" id="mysrpTabContent">
+                <div class="tab-pane fade show active" id="drp-tab-1" role="tabpanel" aria-labelledby="drp-t1"
+                  tabindex="0">
+                  <a href="editar-perfil.php" class="dropdown-item">
+                    <i class="ti ti-edit-circle"></i>
+                    <span>Editar Perfil</span>
+                  </a>
+                  <a href="logout.php" class="dropdown-item">
+                    <i class="ti ti-power"></i>
+                    <span>Logout</span>
+                  </a>
+                </div>
+
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </header>
+  <!-- [ Header ] end -->
+
+  <div class="pc-container">
+    <div class="pc-content">
+
+
+      <!-- [ Main Content ] start -->
+      <div class="row">
+        <div class="col-md-6 col-xl-3">
+          <div class="card">
+            <div class="card-body">
+              <h6 class="mb-2 f-w-400 text-muted">Serviços</h6>
+              <h4 class="mb-3"><?php echo htmlspecialchars($total_servicos); ?></h4>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6 col-xl-3">
+          <div class="card">
+            <div class="card-body">
+              <h6 class="mb-2 f-w-400 text-muted">Transações</h6>
+              <h4 class="mb-3"><?php echo htmlspecialchars($total_transacoes); ?></h4>
+            </div>
+          </div>
+        </div>
+
+        <!-- Required Js -->
+        <script src="../assets/js/plugins/popper.min.js"></script>
+        <script src="../assets/js/plugins/simplebar.min.js"></script>
+        <script src="../assets/js/plugins/bootstrap.min.js"></script>
+        <script src="../assets/js/pcoded.js"></script>
+        <script src="../assets/js/plugins/feather.min.js"></script>
+
+</body>
+<!-- [Body] end -->
+
+</html>
